7d9d1b26c82ac27c5ef332af0ac5a955f7a0f274
putrianastasya <160991485+putrianastasya@users.noreply.github.com>
2024-04-26 21:33:27 +0700

    Add files via upload

---
+++ Restoran/Main.java
@@ -0,0 +1,10 @@
+package Restoran;
+
+public class Main {
+    public static void main(String[] args) {
+
+      
+            Kasir kasir = new Kasir();
+            kasir.jalankan();
+    }
+}
---
+++ Restoran/Pelanggan.java
@@ -0,0 +1,19 @@
+package Restoran;
+
+public class Pelanggan {
+        private String nama;
+    
+        public Pelanggan(String nama) {
+            this.nama = nama;
+        }
+    
+        public String getNama() {
+            return nama;
+        }
+    
+        public void setNama(String nama) {
+            this.nama = nama;
+        }
+    
+    
+}
